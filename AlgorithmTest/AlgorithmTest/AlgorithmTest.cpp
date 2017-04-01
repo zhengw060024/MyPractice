79dfad4269c6b952e0ed82efd7b67982d0061a28
zhengw060024 <zhengwei060024@hotmail.com>
2017-04-01 10:20:03 +0800

    AlgorithmTest
    数据结构和算法测试每日一写。

---
+++ AlgorithmTest/AlgorithmTest/AlgorithmTest.cpp
@@ -0,0 +1,12 @@
+// AlgorithmTest.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "hello world! Algorithm test";
+    return 0;
+}
+
