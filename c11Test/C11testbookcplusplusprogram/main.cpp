3d97ec6ad96cab414518b8371b32d9b484eb237b
zhengw060024 <zhengwei060024@hotmail.com>
2018-01-14 18:02:36 +0800

    测试c11 enum class for in等
    修改

--- c11Test/C11testbookcplusplusprogram/main.cpp
+++ c11Test/C11testbookcplusplusprogram/main.cpp
@@ -73,6 +73,114 @@ int count_charx(char*p, char x) {
 	}
 	return nCount;
 }
+struct Vector
+{
+	int sz;
+	double* elem;
+};
+void vect_init(Vector& v, int s) {
+	v.elem = new double[s];
+	v.sz = s;
+}
+void vect_clear(Vector& v) {
+	if (v.elem) {
+		delete[] v.elem;
+		v.elem = nullptr;
+	}
+}
+double read_and_sum(int s) {
+	Vector v;
+	vect_init(v,s);
+	for (int i = 0; i < s; ++i) {
+		cin >> v.elem[i];
+	}
+	double sum = 0;
+	for (int i = 0; i < s; ++i) {
+		sum += v.elem[i];
+	}
+	vect_clear(v);
+	return sum;
+}
+void runTestStruct(){
+	const double dResult = read_and_sum(3);
+	cout << dResult << endl;
+}
+class Vector_Class
+{
+public:
+	Vector_Class(int s) : elem(new double[s]), sz(s) {
+
+	}
+	double& operator[](int i) {
+		return elem[i];
+	}
+	int size() {
+		return sz;
+	}
+	~Vector_Class() {
+		if (elem) {
+			delete[] elem;
+			elem = nullptr;
+ 		}
+	}
+protected:
+private:
+	double* elem;
+	int sz;
+};
+double read_and_sum_class() {
+	Vector_Class v(3);
+	// int v2[4] = {};
+	for (int i = 0; i != v.size(); ++i)
+	{
+		cin >> v[i];
+	}
+	double sum = 0;
+	for (int i = 0; i != v.size(); ++i) {
+		sum += v[i];
+	}
+	return sum;
+}
+// enum class; in the scope of their enum class
+enum class Traffic_light
+{
+	green,
+	yellow,
+	red
+};
+Traffic_light& operator++(Traffic_light&t) {
+	switch (t)
+	{
+	case Traffic_light::green:
+		return t = Traffic_light::yellow;
+	case Traffic_light::yellow:
+		return t = Traffic_light::red;
+	case Traffic_light::red:
+		return t = Traffic_light::green;
+	}
+}
+void printTrafficColor(Traffic_light light) {
+	switch (light)
+	{
+	case Traffic_light::green:
+		cout << "green" << endl;
+		break;
+	case Traffic_light::yellow:
+		cout << "yellow" << endl;
+		break;
+	case Traffic_light::red:
+		cout << "red" << endl;
+		break;
+	default:
+		break;
+	}
+}
+void test_color(Traffic_light light) {
+	printTrafficColor(light);
+	Traffic_light next = ++light;
+	printTrafficColor(light);
+	printTrafficColor(next);
+}
 int main() {
 	std::cout << "Hello World!" << std::endl;
 	print_square(1.234);
@@ -81,8 +189,13 @@ int main() {
 	autoAndNewForTest();
 	const auto nCount = count_charx("fsdafsdafxxxxxdfdsafxxxxxsdsxsfdd", 'f');
 	cout << nCount << endl;
+	runTestStruct();
+	const double temp2 = read_and_sum_class();
+	cout << temp2 << endl;
+	test_color(Traffic_light::red);
 	const bool temp = accept3();
 	cout << temp << endl;
+
 	getchar();
 
 }
